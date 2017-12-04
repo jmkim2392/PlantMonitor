08c39d18a5a7edbeaa75efc90f7f52db7429fa75
jmkim2392 <jmkim2392@gmail.com>
2017-12-04 11:54:46 -0800

    firebase update test

--- pyrebase1.py
+++ pyrebase1.py
@@ -13,7 +13,8 @@ firebase = pyrebase.initialize_app(config)
 
 humidity= 0
 temperature = 0
-ser = serial.Serial('/dev/ttyACM0', baudrate = 9600, timeout=1) #port name, connect rate, waittime to receive data
+ser = serial.Serial('/dev/ttyACM0', baudrate = 9600, timeout=1) 
+#port name, connect rate, waittime to receive data
 var = 1
 while var == 1 :
     
@@ -24,5 +25,21 @@ timestamp = datetime.datetime.now().strftime("%Y-%m-%d | %H:%M")
 
 int(filter(arduinoData.isdigit, humidity))
 int(filter(arduinoData.isdigit, temperature))
+#store data in to years and months
+#check year
+year = datetime.date.today().strftime("%Y")
+#check month
+month = datetime.date.today().strftime("%B")
+#store date and Time
+timestamp = datetime.datetime.now().strftime("%d | %H:%M")
+db.child(year).child(month).set({
+	'Humidity' :{
+		timestamp : humidity
+	},
+	'Temperature' :{
+		timestamp :temperature
+	}
+})
+
 
 
